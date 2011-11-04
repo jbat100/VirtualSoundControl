1896a815959a0c6f92d98cbe1c37aae67a5c0b87
Jonathan Thorpe <johnny.frenchy@gmail.com>
2011-11-04 16:32:54 +0000

    [GENERIC] Added VSCiOS for iOS specific generic stuff.

---
+++ Generic/VSCiOS.m
@@ -0,0 +1,13 @@
+//
+//  VSCiOS.m
+//  CXPWiFiRemote
+//
+//  Created by Jonathan Thorpe on 11/4/11.
+//  Copyright (c) 2011 NXP. All rights reserved.
+//
+
+#import "VSCiOS.h"
+
+@implementation VSCiOS
+
+@end
