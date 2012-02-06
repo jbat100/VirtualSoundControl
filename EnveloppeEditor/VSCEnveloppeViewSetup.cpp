ffa998b658e677c287a1deb1482e799ef36a823a
Jonathan Thorpe <johnny.frenchy@gmail.com>
2012-02-06 12:06:03 +0100

    Same as previous commit.

--- EnveloppeEditor/VSCEnveloppeViewSetup.cpp
+++ EnveloppeEditor/VSCEnveloppeViewSetup.cpp
@@ -15,145 +15,29 @@
 #pragma mark View range setters / getters
 
 VSCEnveloppeViewSetup::VSCEnveloppeViewSetup(void) {
-	
 	setToDefault();
-	
 }
 
 void VSCEnveloppeViewSetup::setToDefault(void) {
-	
-	setMinTime(0.0);
-	setMaxTime(5.0);
-	setMinValue(0.0);
-	setMaxValue(1.0);
-	
-	setDisplayType(VSCEnveloppeDisplayTypeDefault);
-	
-	setControlPointRadius(3.0);
-	setLineWidth(1.0);
-	
-	setControlPointSelectedColour((VSCColour){0.0, 0.0, 1.0, 1.0});
-	setControlPointUnselectedColour((VSCColour){0.0, 0.0, 0.0, 1.0});
-	setLineColour((VSCColour){0.0, 0.0, 0.0, 1.0});
-	
-	setTargetNumberOfHorizontalGridLines(5);
-	setTargetNumberOfVerticalGridLines(10);
-	
-	setVerticalGridLineColor((VSCColour){0.8, 0.8, 0.8, 1.0});
-	setHorizontalGridLineColor((VSCColour){0.8, 0.8, 0.8, 1.0});
-	setHorizontalZeroGridLineColor((VSCColour){0.5, 0.5, 0.5, 1.0});
-	
-	setVerticalGridLineWidth(1.0);
-	setHorizontalGridLineWidth(1.0);
-	setHorizontalZeroGridLineWidth(1.0);
-	
+	this->setDisplayType(VSCEnveloppeDisplayTypeDefault);
 }
 
 #pragma mark Encveloppe point calculations
 
-int VSCEnveloppeViewSetup::pixelForTime(VSCSFloat t, int widthInPixels) {
+int VSCEnveloppeViewSetup::pixelForTime(VSCSFloat t, int widthInPixels) const {
 	return 0;
 }
 
-int VSCEnveloppeViewSetup::pixelForValue(VSCSFloat t, int heightInPixels) {
+int VSCEnveloppeViewSetup::pixelForValue(VSCSFloat t, int heightInPixels) const {
 	return 0;
 }
 
-VSCSFloat VSCEnveloppeViewSetup::timeForPixel(int pixel, int widthInPixels) {
+VSCSFloat VSCEnveloppeViewSetup::timeForPixel(int pixel, int widthInPixels) const {
 	return 0.0;
 }
 
-VSCSFloat VSCEnveloppeViewSetup::valueForPixel(int pixel, int widthInPixels) {
+VSCSFloat VSCEnveloppeViewSetup::valueForPixel(int pixel, int widthInPixels) const {
 	return 0.0;
-}
-
-#pragma mark Grid point calculations
-
-int VSCEnveloppeViewSetup::getTimeGridPoints(std::list<VSCSFloat>& ptns) {
-	
-	int currentScalerPower = 0;
-	VSCSFloat scaledTimeRange = getMaxTime() - getMinTime();
-	
-	if (scaledTimeRange < getTargetNumberOfVerticalGridLines()) {
-		while (scaledTimeRange*5.0 < getTargetNumberOfVerticalGridLines()) {
-			scaledTimeRange *= 10.0;
-			currentScalerPower += 1;
-		}
-	}
-	
-	else if (scaledTimeRange > getTargetNumberOfVerticalGridLines()) {
-		while (scaledTimeRange*0.5 > getTargetNumberOfVerticalGridLines()) {
-			scaledTimeRange *= 0.1;
-			currentScalerPower -= 1;
-		}
-	}
-	
-	VSCSFloat currentScaler = std::pow(10.0, (VSCSFloat)currentScalerPower);
-	
-	VSCSFloat scaledMinTime = getMinTime() * currentScaler;
-	VSCSFloat scaledMaxTime = getMaxTime() * currentScaler;
-	
-	VSCSFloat roundMinTime = std::floor(scaledMinTime);
-	VSCSFloat roundMaxTime = std::ceil(scaledMaxTime);
-	
-	for (VSCSFloat t = roundMinTime; t < roundMaxTime; t = t+1.0) {
-		ptns.push_back(t/currentScaler);
-	}
-	
-	return currentScalerPower;
-	
-}
-
-int VSCEnveloppeViewSetup::getValueGridPoints(std::list<VSCSFloat>& ptns) {
-	
-	int currentScalerPower = 0;
-	VSCSFloat scaledValueRange = getMaxValue() - getMinValue();
-	
-	if (scaledValueRange < getTargetNumberOfHorizontalGridLines()) {
-		while (scaledValueRange*5.0 < getTargetNumberOfHorizontalGridLines()) {
-			currentScalerPower += 1;
-			scaledValueRange *= 10.0;
-		}
-	}
-	
-	else if (scaledValueRange*0.5 > getTargetNumberOfHorizontalGridLines()) {
-		while (scaledValueRange > getTargetNumberOfHorizontalGridLines()) {
-			currentScalerPower -= 1;
-			scaledValueRange *= 0.1;
-		}
-	}
-	
-	VSCSFloat currentScaler = std::pow(10.0, (VSCSFloat)currentScalerPower);
-	
-	VSCSFloat scaledMinValue = getMinValue() * currentScaler;
-	VSCSFloat scaledMaxValue = getMaxValue() * currentScaler;
-	
-	VSCSFloat roundMinValue = std::floor(scaledMinValue);
-	VSCSFloat roundMaxValue = std::ceil(scaledMaxValue);
-	
-	for (VSCSFloat v = roundMinValue; v < roundMaxValue; v = v+1.0) {
-		ptns.push_back(v/currentScaler);
-	}
-	
-	return currentScalerPower;
-	
-}
-
-int VSCEnveloppeViewSetup::getTimeGridPointsAndPixels(std::list< std::pair<VSCSFloat,int> >& pps, int pixelSize) {
-	
-	std::list<VSCSFloat> points;
-	getTimeGridPoints(points);
-	
-	return 0;
-	
-}
-
-int VSCEnveloppeViewSetup::getValueGridPointsAndPixels(std::list< std::pair<VSCSFloat,int> >& pps, int pixelSize) {
-	
-	std::list<VSCSFloat> points;
-	getValueGridPoints(points);
-	
-	return 0;
 }
 
 #pragma mark View range setters / getters
@@ -161,39 +45,40 @@ int VSCEnveloppeViewSetup::getValueGridPointsAndPixels(std::list< std::pair<VSCS
 /*
  *	View range setters / getters
  */
-
-float VSCEnveloppeViewSetup::getMinTime(void) const {
-	return _minTime;
+VSCEnveloppe::TimeRange VSCEnveloppeViewSetup::getTimeRange(void) const {
+    return _timeRange;
 }
 
-void VSCEnveloppeViewSetup::setMinTime(float  minTime) {
-	_minTime = minTime;
+void VSCEnveloppeViewSetup::setTimeRange(VSCEnveloppe::TimeRange timeRange) {
+    _timeRange = timeRange;
 }
 
-
-float VSCEnveloppeViewSetup::getMaxTime(void) const {
-	return _maxTime;
+VSCEnveloppe::ValueRange VSCEnveloppeViewSetup::getValueRange(void) const {
+    return _valueRange;
 }
 
-void VSCEnveloppeViewSetup::setMaxTime(float  maxTime) {
-	_maxTime = maxTime;
+void VSCEnveloppeViewSetup::setValueRange(VSCEnveloppe::ValueRange valueRange) {
+    _valueRange = valueRange;
 }
 
-float VSCEnveloppeViewSetup::getMinValue(void) const {
-	return _minValue;
+/*
+ *	Allowed range setters / getters
+ */
+
+VSCEnveloppe::TimeRange VSCEnveloppeViewSetup::getAllowedTimeRange(void) const {
+    return _allowedTimeRange;
 }
 
-void VSCEnveloppeViewSetup::setMinValue(float  minValue) {
-	_minValue = minValue;
+void VSCEnveloppeViewSetup::setAllowedTimeRange(VSCEnveloppe::TimeRange timeRange) {
+    _allowedTimeRange = timeRange;
 }
 
-
-float VSCEnveloppeViewSetup::getMaxValue(void) const {
-	return _maxValue;
+VSCEnveloppe::ValueRange VSCEnveloppeViewSetup::getAllowedValueRange(void) const {
+    return _allowedValueRange;
 }
 
-void VSCEnveloppeViewSetup::setMaxValue(float  maxValue) {
-	_maxValue = maxValue;
+void VSCEnveloppeViewSetup::setAllowedValueRange(VSCEnveloppe::ValueRange valueRange) {
+    _allowedValueRange = valueRange;
 }
 
 #pragma mark Display type setters / getters
@@ -209,152 +94,5 @@ VSCEnveloppeDisplayType VSCEnveloppeViewSetup::getDisplayType(void) const {
 void VSCEnveloppeViewSetup::setDisplayType(VSCEnveloppeDisplayType displayType) {
 	_displayType = displayType;
 }
-
-#pragma mark Control Points setters / getters
-
-/*
- *	Control Points setters / getters
- */
-
-float VSCEnveloppeViewSetup::getControlPointRadius(void) const {
-	return _controlPointRadius;
-}
-
-void VSCEnveloppeViewSetup::setControlPointRadius(float controlPointRadius) {
-	_controlPointRadius = controlPointRadius;
-}
-
-VSCColour VSCEnveloppeViewSetup::getControlPointSelectedColour(void) const {
-	return _controlPointSelectedColour;
-}
-
-void VSCEnveloppeViewSetup::setControlPointSelectedColour(VSCColour colour) {
-	_controlPointSelectedColour = colour;
-}
-
-VSCColour VSCEnveloppeViewSetup::getControlPointUnselectedColour(void) const {
-	return _controlPointUnselectedColour;
-}
-
-void VSCEnveloppeViewSetup::setControlPointUnselectedColour(VSCColour colour) {
-	_controlPointUnselectedColour = colour;
-}
-
-float VSCEnveloppeViewSetup::getLineWidth(void) const {
-	return _lineWidth;
-}
-
-void VSCEnveloppeViewSetup::setLineWidth(float lineWidth) {
-	_lineWidth = lineWidth;
-}
-
-#pragma mark Enveloppe lines setters / getters
-
-/*
- *	Enveloppe lines setters / getters
- */
-
-VSCColour VSCEnveloppeViewSetup::getLineColour(void) const {
-	return _lineColour;
-}
-
-void VSCEnveloppeViewSetup::setLineColour(VSCColour colour) {
-	_lineColour = colour;
-}
-
-/*
- *	Grid setters / getters
- */
-
-bool VSCEnveloppeViewSetup::gridIsShowing(void) const {
-	return _showGrid;
-}
-
-void VSCEnveloppeViewSetup::showGrid(bool showGrid) {
-	_showGrid = showGrid;
-}
-
-float VSCEnveloppeViewSetup::getTargetVerticalGridSpacing(void) const {
-	return _targetVerticalGridSpacing;
-}
-
-void VSCEnveloppeViewSetup::setTargetVerticalGridSpacing(float targetVerticalGridSpacing) {
-	_targetVerticalGridSpacing = targetVerticalGridSpacing;
-}
-
-float VSCEnveloppeViewSetup::getTargetHorizontalGridSpacing(void) const {
-	return _targetHorizontalGridSpacing;
-}
-
-void VSCEnveloppeViewSetup::setTargetHorizontalGridSpacing(float targetHorizontalGridSpacing) {
-	_targetHorizontalGridSpacing = targetHorizontalGridSpacing;
-}
-
-float VSCEnveloppeViewSetup::getTargetNumberOfVerticalGridLines(void) const {
-	return _targetNumberOfVerticalGridLines;
-}
-
-void VSCEnveloppeViewSetup::setTargetNumberOfVerticalGridLines(float targetNumberOfVerticalGridLines) {
-	_targetNumberOfVerticalGridLines = targetNumberOfVerticalGridLines;
-}
-
-float VSCEnveloppeViewSetup::getTargetNumberOfHorizontalGridLines(void) const {
-	return _targetNumberOfHorizontalGridLines;
-}
-
-void VSCEnveloppeViewSetup::setTargetNumberOfHorizontalGridLines(float targetNumberOfHorizontalGridLines) {
-	_targetNumberOfHorizontalGridLines = targetNumberOfHorizontalGridLines;
-}
-
-
-float VSCEnveloppeViewSetup::getVerticalGridLineWidth(void) const {
-	return _verticalGridLineWidth;
-}
-
-void VSCEnveloppeViewSetup::setVerticalGridLineWidth(float verticalGridLineWidth) {
-	_verticalGridLineWidth = verticalGridLineWidth;
-}
-
-float VSCEnveloppeViewSetup::getHorizontalGridLineWidth(void) const {
-	return _horizontalGridLineWidth;
-}
-
-void VSCEnveloppeViewSetup::setHorizontalGridLineWidth(float horizontalGridLineWidth) {
-	_horizontalGridLineWidth = horizontalGridLineWidth;
-}
-
-float VSCEnveloppeViewSetup::getHorizontalZeroGridLineWidth(void) const {
-	return _horizontalZeroGridLineWidth;
-}
-
-void VSCEnveloppeViewSetup::setHorizontalZeroGridLineWidth(float horizontalZeroGridLineWidth) {
-	_horizontalZeroGridLineWidth = horizontalZeroGridLineWidth;
-}
-
-
-VSCColour VSCEnveloppeViewSetup::getHorizontalGridLineColor(void) const {
-	return _horizontalGridLineColor;
-}
-
-void VSCEnveloppeViewSetup::setHorizontalGridLineColor(VSCColour horizontalGridLineColor) {
-	_horizontalGridLineColor = horizontalGridLineColor;
-}
-
-VSCColour VSCEnveloppeViewSetup::getHorizontalZeroGridLineColor(void) const {
-	return _horizontalZeroGridLineColor;
-}
-
-void VSCEnveloppeViewSetup::setHorizontalZeroGridLineColor(VSCColour horizontalZeroGridLineColor) {
-	_horizontalZeroGridLineColor = horizontalZeroGridLineColor;
-}
-
-VSCColour VSCEnveloppeViewSetup::getVerticalGridLineColor(void) const {
-	return _verticalGridLineColor;
-}
-
-void VSCEnveloppeViewSetup::setVerticalGridLineColor(VSCColour verticalGridLineColor) {
-	_verticalGridLineColor = verticalGridLineColor;
-}
-
 
 
--- EnveloppeEditor/VSCEnveloppeViewSetup.h
+++ EnveloppeEditor/VSCEnveloppeViewSetup.h
@@ -12,6 +12,8 @@
 
 #include "VSCColour.h"
 #include "VSCSound.h"
+#include "VSCEnveloppe.h"
+#include "VSCEnveloppeDisplaySetup.h"
 #include <utility>
 #include <list>
 #include <boost/shared_ptr.hpp>
@@ -36,14 +38,18 @@ public:
 	/*
 	 *	View range setters / getters
 	 */
-	float getMinTime(void) const;
-	void setMinTime(float  minTime);
-	float getMaxTime(void) const;
-	void setMaxTime(float  maxTime);
-	float getMinValue(void) const;
-	void setMinValue(float  minValue);
-	float getMaxValue(void) const;
-	void setMaxValue(float  maxValue);
+    VSCEnveloppe::TimeRange getTimeRange(void) const;
+	void setTimeRange(VSCEnveloppe::TimeRange timeRange);
+	VSCEnveloppe::ValueRange getValueRange(void) const;
+	void setValueRange(VSCEnveloppe::ValueRange valueRange);
+    
+    /*
+	 *	Allowed range setters / getters
+	 */
+    VSCEnveloppe::TimeRange getAllowedTimeRange(void) const;
+	void setAllowedTimeRange(VSCEnveloppe::TimeRange timeRange);
+	VSCEnveloppe::ValueRange getAllowedValueRange(void) const;
+	void setAllowedValueRange(VSCEnveloppe::ValueRange valueRange);
 	
 	/*
 	 *	Display type setters / getters
@@ -51,73 +57,18 @@ public:
 	VSCEnveloppeDisplayType getDisplayType(void) const;
 	void setDisplayType(VSCEnveloppeDisplayType displayType);
 	
-	/*
-	 *	Control Points setters / getters
-	 */
-	float getControlPointRadius(void) const;
-	void setControlPointRadius(float controlPointRadius);
-	VSCColour getControlPointSelectedColour(void) const;
-	void setControlPointSelectedColour(VSCColour colour);
-	VSCColour getControlPointUnselectedColour(void) const;
-	void setControlPointUnselectedColour(VSCColour colour);
-	
-	/*
-	 *	Enveloppe lines setters / getters
-	 */
-	float getLineWidth(void) const;
-	void setLineWidth(float lineWidth);
-	VSCColour getLineColour(void) const;
-	void setLineColour(VSCColour colour);
     
     /*
 	 *	Enveloppe point calculations
 	 */
 	
-	int pixelForTime(VSCSFloat t, int widthInPixels);
-	int pixelForValue(VSCSFloat t, int heightInPixels);
-	VSCSFloat timeForPixel(int pixel, int widthInPixels);
-	VSCSFloat valueForPixel(int pixel, int widthInPixels);
+	int pixelForTime(VSCSFloat t, int widthInPixels) const;
+	int pixelForValue(VSCSFloat t, int heightInPixels) const;
+	VSCSFloat timeForPixel(int pixel, int widthInPixels) const;
+	VSCSFloat valueForPixel(int pixel, int widthInPixels) const;
 	
-	/*-------------------------------------------------------------------
-     *  Grid stuff not implemented 
-	 *	Grid setters / getters
-	 */
-	
-	bool gridIsShowing(void) const;
-	void showGrid(bool showGrid);
-	
-	float getTargetVerticalGridSpacing(void) const;
-	void setTargetVerticalGridSpacing(float targetVerticalGridSpacing);
-	float getTargetHorizontalGridSpacing(void) const;
-	void setTargetHorizontalGridSpacing(float targetHorizontalGridSpacing);
-	
-	float getTargetNumberOfVerticalGridLines(void) const;
-	void setTargetNumberOfVerticalGridLines(float targetNumberOfVerticalGridLines);
-	float getTargetNumberOfHorizontalGridLines(void) const;
-	void setTargetNumberOfHorizontalGridLines(float targetNumberOfHorizontalGridLines);
-	
-	float getVerticalGridLineWidth(void) const;
-	void setVerticalGridLineWidth(float verticalGridLineWidth);
-	float getHorizontalGridLineWidth(void) const;
-	void setHorizontalGridLineWidth(float horizontalGridLineWidth);
-	float getHorizontalZeroGridLineWidth(void) const;
-	void setHorizontalZeroGridLineWidth(float horizontalZeroGridLineWidth);
-	
-	VSCColour getHorizontalGridLineColor(void) const;
-	void setHorizontalGridLineColor(VSCColour horizontalGridLineColor);
-	VSCColour getHorizontalZeroGridLineColor(void) const;
-	void setHorizontalZeroGridLineColor(VSCColour horizontalZeroGridLineColor);
-	VSCColour getVerticalGridLineColor(void) const;
-	void setVerticalGridLineColor(VSCColour verticalGridLineColor);
-	
-	/*
-	 *	Grid point calculations
-	 */
-	
-	int getTimeGridPoints(std::list<VSCSFloat>& ptns);
-	int getValueGridPoints(std::list<VSCSFloat>& ptns);
-	int getTimeGridPointsAndPixels(std::list< std::pair<VSCSFloat,int> >& pps, int widthInPixels);
-	int getValueGridPointsAndPixels(std::list< std::pair<VSCSFloat,int> >& pps, int heightInPixels);
+    VSCEnveloppeDisplaySetup& getActiveDisplaySetup();
+	VSCEnveloppeDisplaySetup& getInactiveDisplaySetup();
 	
 	
 private:
@@ -125,44 +76,19 @@ private:
 	/*
 	 *	View range
 	 */
-	float _minTime;
-	float _maxTime;
-	float _minValue;
-	float _maxValue;
+	VSCEnveloppe::TimeRange _timeRange;
+	VSCEnveloppe::ValueRange _valueRange;
+    
+    /*
+	 *	Allowed range
+	 */
+	VSCEnveloppe::TimeRange _allowedTimeRange;
+	VSCEnveloppe::ValueRange _allowedValueRange;
 	
 	/*
 	 *	Display type
 	 */
 	VSCEnveloppeDisplayType _displayType;
-	
-	/*
-	 *	Control Points
-	 */
-	float _controlPointRadius;
-	VSCColour _controlPointSelectedColour;
-	VSCColour _controlPointUnselectedColour;
-	
-	/*
-	 *	Enveloppe lines
-	 */
-	float _lineWidth;
-	VSCColour _lineColour;
-	
-	/*
-	 *	Grid
-	 */
-	bool _showGrid;
-	unsigned int _targetNumberOfVerticalGridLines;
-	unsigned int _targetNumberOfHorizontalGridLines;
-	float _targetVerticalGridSpacing;
-	float _targetHorizontalGridSpacing;
-	float _verticalGridLineWidth;
-	float _horizontalGridLineWidth;
-	float _horizontalZeroGridLineWidth;
-	VSCColour _horizontalGridLineColor;
-	VSCColour _horizontalZeroGridLineColor;
-	VSCColour _verticalGridLineColor;
-	
 
 };
 
