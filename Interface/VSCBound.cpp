f4e9db9867c2525c18149d3cc3e540c1ecd990ca
Jonathan Thorpe <johnny.frenchy@gmail.com>
2012-09-13 10:16:32 +0200

    [OGREBULLET] Updated Xcode project.

---
+++ Interface/VSCBound.cpp
@@ -0,0 +1,70 @@
+
+#include "VSCKeyBindings.h"
+#include "VSCException.h"
+
+const VSCKeyboard::Action VSCKeyBindings::getActionForCombination(const VSCKeyboard::Combination& comb) const
+{
+    CombinationConstIterator it = mCombinationActionMap.left.find(comb);
+    
+    if (it != mCombinationActionMap.left.end())
+    {
+        return it->second;
+    }
+        
+    return VSCKeyboard::NullAction;
+}
+
+const VSCKeyboard::Combination VSCKeyBindings::getCombinationForAction(const VSCKeyboard::Action& action) const
+{
+    ActionConstIterator it = mCombinationActionMap.right.find(action);
+    
+    if (it != mCombinationActionMap.right.end())
+    {
+        return it->second;
+    }
+    
+    return VSCKeyboard::NullCombination;
+}
+
+void VSCKeyBindings::eraseCombination(const VSCKeyboard::Combination& comb)
+{
+    mCombinationActionMap.left.erase(comb);
+}
+
+void VSCKeyBindings::eraseAction(const VSCKeyboard::Action& action)
+{
+    mCombinationActionMap.right.erase(action);
+}
+
+void VSCKeyBindings::setBinding(const VSCKeyboard::Action& action, const VSCKeyboard::Combination& comb)
+{
+    
+    /*
+     *  Check if the combination is already bound
+     */
+    
+    CombinationConstIterator combIt = mCombinationActionMap.left.find(comb);
+    
+    if (combIt != mCombinationActionMap.left.end())
+    {
+        throw VSCUIException("Attempted to set binding with already bound combination");
+    }
+    
+    /*
+     *  Check if the action is already bound
+     */
+    
+    ActionConstIterator actionIt = mCombinationActionMap.right.find(action);
+    
+    if (actionIt != mCombinationActionMap.right.end())
+    {
+        throw VSCUIException("Attempted to set binding with already bound action");
+    }
+    
+    mCombinationActionMap.insert(CombinationActionMapEntry(comb, action));
+    
+}
+
+
+
+
---
+++ Interface/VSCBound.h
@@ -0,0 +1,57 @@
+
+#ifndef _VSC_KEY_BINDINGS_H_
+#define _VSC_KEY_BINDINGS_H_
+
+#include "VSCUserInput.h"
+#include <boost/bimap.hpp>
+#include <string>
+#include <set>
+#include "OIS.h"
+
+/*
+ *  VSCKeyBindings stores and manipulates a group of bindings between VSCKeyboard::Action and 
+ *  VSCKeyboard::Combination
+ */
+
+class VSCKeyBindings 
+{
+    
+public:
+    
+    /*
+     *  Get the Action/Combination associated with a Combination/Action. Returns NullAction/NullCombination
+     *  if none was found.
+     */
+    
+    const VSCKeyboard::Action getActionForCombination(const VSCKeyboard::Combination& comb) const;
+    const VSCKeyboard::Combination getCombinationForAction(const VSCKeyboard::Action& action) const;
+    
+    /*
+     *  Erase the binding for a given Action or Combination. 
+     */
+    
+    void eraseCombination(const VSCKeyboard::Combination& comb);
+    void eraseAction(const VSCKeyboard::Action& action);
+    
+    /*
+     *  Set a binding, throws an exception if the action and/or the combination is already bound.
+     */
+    
+    void setBinding(const VSCKeyboard::Action& action, const VSCKeyboard::Combination& comb);
+    
+private:
+    
+    typedef boost::bimap<VSCKeyboard::Combination, VSCKeyboard::Action>     CombinationActionMap;
+    typedef CombinationActionMap::value_type                                CombinationActionMapEntry;
+    typedef CombinationActionMap::left_map::iterator                        CombinationIterator;
+    typedef CombinationActionMap::right_map::iterator                       ActionIterator;
+    typedef CombinationActionMap::left_map::const_iterator                  CombinationConstIterator;
+    typedef CombinationActionMap::right_map::const_iterator                 ActionConstIterator;
+    
+    CombinationActionMap    mCombinationActionMap;
+    
+};
+
+
+#endif//_VSC_KEY_BINDINGS_H_
+
